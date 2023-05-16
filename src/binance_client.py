4333bf7b55cb3b88ffbf53aaf823f4e3227fbe3e
fez29 <41666396+Fez29@users.noreply.github.com>
2023-05-16 10:16:29 +0200

    removed api keys from client

--- src/binance_client.py
+++ src/binance_client.py
@@ -1,15 +1,4 @@
 from binance.client import Client
 from api_keys import api_key, api_secret
 
-testnet = True
-
-# insert your api keys here
-if testnet == True:
-    # Testnet
-    api_key = "HZcMh0e21m9HDNaElOYIpBZJ16xoVE25SqjXbmXrfXq6P9fS0kj8yUBiqbjcgM3h"
-    api_secret = "oK4pSQLbBXA98TFodCIfNhV6gfL2w69ZjXsc6c0eOVzIvmzWhcICppFgLlJGV7qH"
-else:
-    api_key = "your_api_key"
-    api_secret = "your_api_secret"
-
 client = Client(api_key, api_secret)
